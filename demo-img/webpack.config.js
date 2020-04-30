61f23863898826b9436d7a76eade2b9d062555c1
zhangwei <st_tgy927@163.com>
2020-05-01 01:29:27 +0800

    这是三体的v1版本

---
+++ demo-img/webpack.config.js
@@ -0,0 +1,21 @@
+const {resolve}=require('path');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+module.exports={
+    entry:'./src/index.js',
+    output:{
+        filename:'built.js',
+        path:resolve(__dirname,'build')
+    },
+    module:{
+        rules:[
+
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./temp/index.html'
+        })
+    ],
+    mode:'development'
+    // mode:'production'
+}
